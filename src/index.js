0bd1c877c77c15095337dc432d41caaf32677b0e
atr <atr@ciklum.com>
2018-02-21 15:38:45 +0300

    Quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    equation = equation.replace(/\s/g, "");
+    var reg = /(.+)\*x\^2(.+)\*x(.+)/g,
+        equationArr = reg.exec(equation),
+        a = +equationArr[1],
+        b = +equationArr[2],
+        c = +equationArr[3],
+        x1,
+        x2;
+
+    var d = b*b - 4*a*c;
+
+    if (d < 0) {
+        throw ('No valid results');
+
+        breake;
+    };
+
+    if (d > 0) {
+        x1 = Math.round((-b - Math.sqrt(d))/(2*a));
+        x2 = Math.round((-b + Math.sqrt(d))/(2*a));
+
+        return [x1,x2].sort(function (a2, b2) {return a2 - b2});
+    }
+
+    if (d = 0) {
+        x1 = x2 = Math.round(-b/(2*a));
+
+        return [x1,x2];
+    };
+};
